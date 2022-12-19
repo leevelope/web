a0895ed2b4d7a5b097160143616281a8a81d8462
leevelope <119020420+leevelope@users.noreply.github.com>
2022-12-19 10:44:54 +0900

    js수정

--- js/index.js
+++ js/index.js
@@ -135,3 +135,51 @@ bottomContact모달닫기창();
 // }
 
 // 시작줄();
+
+
+// $('#start').toggle(function () {
+//   $('.alert').display('block');
+// }, function () {
+//   $('.alert').display('none');
+// });
+
+$(function () {
+  $("#start").click(function () {
+    $(".alert").toggle();
+  });
+});
+
+// alert버튼이 눌러져있을 때 바탕화면을 클릭해도 display none을 하려면?
+// $('#top').on('click', function () {
+//   if (document.querySelector('.class').display == 'block') {
+//     display('none');
+//   }
+// });
+
+
+
+var typingBool = false;
+var typingIdx = 0;
+
+// 타이핑될 텍스트를 가져온다 
+var typingTxt = $(".typing-txt").text();
+
+typingTxt = typingTxt.split(""); // 한글자씩 자른다. 
+
+if (typingBool == false) {
+  // 타이핑이 진행되지 않았다면 
+  typingBool = true;
+  var tyInt = setInterval(typing, 100); // 반복동작 
+}
+
+function typing() {
+  if (typingIdx < typingTxt.length) {
+    // 타이핑될 텍스트 길이만큼 반복 
+    $(".typing").append(typingTxt[typingIdx]);
+    // 한글자씩 이어준다. 
+    typingIdx++;
+  } else {
+    //끝나면 반복종료 
+    clearInterval(tyInt);
+  }
+}  
